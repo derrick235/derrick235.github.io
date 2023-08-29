a3eba287fe142d45241d24663db3f7c1991423fb
Derrick Lu <44071958+derrick235@users.noreply.github.com>
2023-08-29 17:54:27 -0400

    Add Contact page
    Add Contact page with links to GitHub, LinkedIn, and email.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const buttons = document.querySelectorAll(".button");
+const navbarButtons = document.querySelectorAll(".navbar-button");
+const pageButtons = document.querySelectorAll(".black-button");
 
 function addButtonAnimation() {
     this.classList.add("button-hover");
@@ -8,11 +9,28 @@ function removeButtonAnimation() {
     this.classList.remove("button-hover");
 }
 
-buttons.forEach((button) => {
+function addBlueButtonAnimation() {
+    this.classList.add("button-hover-blue");
+}
+
+function removeBlueButtonAnimation() {
+    this.classList.remove("button-hover-blue");
+}
+
+navbarButtons.forEach((button) => {
 
     if (!(button.classList.contains("button-hover"))) {
         button.addEventListener("mouseover", addButtonAnimation);
         button.addEventListener("mouseout", removeButtonAnimation);
+    }
+
+})
+
+pageButtons.forEach((button) => {
+
+    if (!(button.classList.contains("button-hover"))) {
+        button.addEventListener("mouseover", addBlueButtonAnimation);
+        button.addEventListener("mouseout", removeBlueButtonAnimation);
     }
 
 })
